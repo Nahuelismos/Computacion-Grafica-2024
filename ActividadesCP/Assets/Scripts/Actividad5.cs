47646190196aab57053d9ea85cd76b2534f86bbd
Nahuel Chaves <nahuel_chaves_98@hotmail.com>
2024-03-21 10:37:48 -0300

    Actividad5
    Finiquitada la activdad5

--- ActividadesCP/Assets/Scripts/Actividad5.cs
+++ ActividadesCP/Assets/Scripts/Actividad5.cs
@@ -8,17 +8,61 @@ public class Actividad5 : MonoBehaviour
     private int[] triangles;
     private GameObject[] objetoF;
     private GameObject miCamara;
+    private int cant = 12;
 
     void Start()
     {
-	CreateModel();
-	for(int i = 0;i<1; i++){
-        	objetoF[i] = new GameObject("LetraF"+i);
+		CreateModel(); 
+        objetoF = new GameObject[cant];
+		for(int i = 0;i<cant; i++){
+            string n = "LetraF"+i;
+        	objetoF[i] = new GameObject(n);
         	objetoF[i].AddComponent<MeshFilter>();
         	objetoF[i].GetComponent<MeshFilter>().mesh = new Mesh();
         	objetoF[i].AddComponent<MeshRenderer>();	
         	UpdateMesh(i);
-	}
+	    }
+        //objeto1
+        objetoF[1].transform.position = new Vector3(1,0,1);
+        objetoF[1].transform.rotation = Quaternion.Euler(0,180,0);
+        
+        //objeto2
+        objetoF[2].transform.position = new Vector3(1,0,1);
+        objetoF[2].transform.rotation = Quaternion.Euler(0,-90,0);
+
+        //objeto3
+        objetoF[3].transform.position = new Vector3(0,0,2);
+        objetoF[3].transform.rotation = Quaternion.Euler(0,90,0);
+
+        //objeto4
+        objetoF[4].transform.position = new Vector3(0,0,2);
+
+        //objeto5
+        objetoF[5].transform.position = new Vector3(1,0,3);
+        objetoF[5].transform.rotation = Quaternion.Euler(0,180,0);
+        
+        //objeto6
+        objetoF[6].transform.position = new Vector3(1,0,3);
+        objetoF[6].transform.rotation = Quaternion.Euler(0,-90,0);
+
+        //objeto7
+        objetoF[7].transform.position = new Vector3(0,0,4);
+        objetoF[7].transform.rotation = Quaternion.Euler(0,90,0);
+
+        //objeto8
+        objetoF[8].transform.position = new Vector3(1,0,1);
+
+        //objeto9
+        objetoF[9].transform.position = new Vector3(2,0,2);
+        objetoF[9].transform.rotation = Quaternion.Euler(0,180,0);
+
+        //objeto10
+        objetoF[10].transform.position = new Vector3(1,0,3);
+
+        //objeto11
+        objetoF[11].transform.position = new Vector3(2,0,4);
+        objetoF[11].transform.rotation = Quaternion.Euler(0,180,0);
+
         CreateMaterial();
         CreateCamera();
     }
@@ -28,26 +72,23 @@ public class Actividad5 : MonoBehaviour
       
     }
 
-    private void CreateModel()
-    {
+    private void CreateModel() {
         vertices = new Vector3[]{
             new Vector3(0,0,0),     //0
             new Vector3(0,0,1),     //1
-	    new Vector3(1,0,0)	    //2
+	    	new Vector3(1,0,0)	    //2
         };
         triangles = new int[]{
             0,1,2 //t1
         };
     }
 
-    private void UpdateMesh(int i)
-    {
+    private void UpdateMesh(int i) {
         objetoF[i].GetComponent<MeshFilter>().mesh.vertices = vertices;
         objetoF[i].GetComponent<MeshFilter>().mesh.triangles = triangles;
     }
 
-    private void CreateCamera()
-    {
+    private void CreateCamera(){
         miCamara = new GameObject("Camara");
         miCamara.AddComponent<Camera>();
 
@@ -56,14 +97,13 @@ public class Actividad5 : MonoBehaviour
 
         miCamara.transform.rotation = Quaternion.Euler(90, 0, 0);
         miCamara.GetComponent<Camera>().clearFlags = CameraClearFlags.SolidColor;
-        miCamara.GetComponent<Camera>().backgroundColor = Color.black;
+        miCamara.GetComponent<Camera>().backgroundColor = Color.white;
     }
 
-    private void CreateMaterial()
-    {
+    private void CreateMaterial() {
         Material newMaterial = new Material(Shader.Find("Standard"));
-	for(int i = 0;i<1;i++){
+		for(int i = 0;i<cant;i++){
         	objetoF[i].GetComponent<MeshRenderer>().material = newMaterial;
-	}
+		}
     }
 }

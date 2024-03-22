faef789a6fe69801dff10ec8c3da1d2f8a7ff99f
Nahuelismos <nahuel_chaves_98@hotmail.com>
2024-03-22 20:42:19 -0300

    Actividad 6
    Todavia no sale >:c

--- ActividadesCP/Assets/Scripts/Actividad6.cs
+++ ActividadesCP/Assets/Scripts/Actividad6.cs
@@ -5,36 +5,48 @@ using UnityEngine;
 public class Actividad6 : MonoBehaviour
 {
     // Start is called before the first frame update
-    public Vector2 mouse, mousePress;
-    public float rotacion;
-    private GameObject miCamara;
-    public Vector3 posicionCamara, rotacionCamara; 
+    [SerializeField] private  float mouse, mousePress, mouseMov;
+    [SerializeField] private float rotacion;
+    [SerializeField] private GameObject miCamara;
+    [SerializeField] private Vector3 posicionCamara, rotacionCamara; 
     void Start(){
-        CreateCamera();
         posicionCamara = new Vector3(0,0,-7);
         rotacionCamara = new Vector3(0,0,0);
+        CreateCamera();
     }
 
     // Update is called once per frame
-    void Update()
+    /*void Update()
     {
-        mouse = Input.mousePosition;
+        mouse = Input.mousePosition; //solo para ver
         if (Input.GetKeyDown(KeyCode.Mouse0)){
             mousePress = mouse;
         }
         if(Input.GetKey(KeyCode.Mouse0)){
-            if(mousePress.x != 0.0f)
-                rotacion = ((mousePress.x-mouse.x)/mousePress.x);
-            miCamara.transform.Rotate(0.0f,rotacion,0.0f,Space.World); 
+            mouseMov = mousePress - mouse;
+            //miCamara.transform.Rotate(0.0f, mouseMov.x/100, 0.0f,Space.World);
+
         }
         if(Input.GetKeyUp(KeyCode.Mouse0)){
-            miCamara.transform.Rotate(0.0f,rotacion,0.0f,Space.World); 
-            //miCamara.transform.rotation = Quaternion.Euler(rotacionCamara.x,rotacionCamara.y+rotacion,rotacionCamara.z);
+            mouseMov = new Vector3(0, 0, 0);
         }
-        miCamara.transform.position = posicionCamara;
-        //miCamara.transform.Rotate(0.0f,rotacion,0.0f,Space.World); 
+        miCamara.transform.LookAt(Vector3.zero);
+        rotacionCamara = new Vector3 (miCamara.transform.rotation.x, miCamara.transform.rotation.y, miCamara.transform.rotation.z);
+    }*/
+
+    private void Update() {
+        mouse = Input.mousePosition.x;
+        if (Input.GetKeyDown(KeyCode.Mouse0)) {
+            mousePress = Input.mousePosition.x;
+        }
+        if (Input.GetKey(KeyCode.Mouse0)) {
+            mouseMov = Mathf.Clamp((mousePress - mouse), -100f,100f);
+       
+        }
+        miCamara.transform.rotation = Quaternion.Euler(0, mouseMov, 0);
+
     }
-      private void CreateCamera()
+    private void CreateCamera()
     {
         miCamara = new GameObject("Camara");
         miCamara.AddComponent<Camera>();

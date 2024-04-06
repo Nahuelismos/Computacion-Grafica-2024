aa9b59c875b0195129a518e063f16038751b6e8e
Nahuel Chaves <nahuel_chaves_98@hotmail.com>
2024-04-06 15:40:34 -0300

    Actualizacion actividad10
    Se puede guardar los vertices y triangulos

--- ActividadesCP/Assets/Scripts/Clase3.cs
+++ ActividadesCP/Assets/Scripts/Clase3.cs
@@ -3,20 +3,33 @@ using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 
-public class Clase3 : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start(){
-		string fileName = "Bed1";
+public class Clase3 : MonoBehaviour {
+	
+	[SerializeField] private Vector3[] vertices;
+    [SerializeField] private int[] triangles;
+	private Color[] colores;
+    private  GameObject objeto;
+	void Start(){
+		string fileName = "Cubo";
 		string path = "Assets/Objects/"+fileName+".obj";
 		StreamReader reader = new StreamReader(path);
+		
 		string fileData = (reader.ReadToEnd());
-		ReadEachLine(fileData);
+		ReadEachLine(fileData);//create model
+		
+		
+		objeto = new GameObject(fileName);
+        objeto.AddComponent<MeshFilter>();
+        objeto.GetComponent<MeshFilter>().mesh = new Mesh();
+        objeto.AddComponent<MeshRenderer>();
+		
+		UpdateMesh();
+        CreateMaterial();
 		reader.Close();
 		Debug.Log(fileData);
+		
     }
 
-    // Update is called once per frame
     void Update() {
         
     }
@@ -25,47 +38,80 @@ public class Clase3 : MonoBehaviour
 		string[] lines = fileData.Split('\n');
 		int cantV = 0;
 		int cantF = 0;
+		List<Vector3> lista_vertice = new List<Vector3>();
+		List<Vector3> lista_triangulo = new List<Vector3>();
 		for(int i = 0; i< lines.Length; i++){
 			//Debug.Log(lines[i]);
 			if(lines[i].StartsWith("v ")){
-				//es un vector_string
-				float[] vector_float = new float[3];
-				string vector_string = " ";
-				int pos_vector = 0;
-				for(int j = 2; j< lines[i].Length; j++){
-					if((lines[i][j] != ' ')){
-						vector_string+=lines[i][j];
-					} else{
-						//Debug.Log(cantV+"-"+pos_vector+": "+vector_string);
-						vector_float[pos_vector] = float.Parse(vector_string.Split(".")[0]) + float.Parse(vector_string.Split(".")[1])*Mathf.Pow(10,(-1)*vector_string.Split(".")[1].Length);
-						pos_vector++;
-						vector_string = "";
-					}
-				}
-				//Debug.Log(cantV+"-"+pos_vector+": "+vector_string+"---");
-				vector_float[pos_vector] = float.Parse(vector_string.Split(".")[0]) + float.Parse(vector_string.Split(".")[1])*Mathf.Pow(10,(-1)*(vector_string.Split(".")[1].Length-1));
-				//Debug.Log(cantV+": <"+vector_float[0]+" - "+vector_float[1]+" - "+vector_float[2]+">");
-				cantV++;
-			} else if (lines[i].StartsWith("f ")){
-				//es una cara
-				int[] vertice_int = new int[9];
+				//es un vertice_string
+				float[] vertice_float = new float[3];
 				string vertice_string = " ";
 				int pos_vertice = 0;
-				for(int j = 3; j< lines[i].Length; j++){
-					if((lines[i][j] != ' ')	 && (lines[i][j] != '/')){
+				for(int j = 2; j< lines[i].Length; j++){
+					if((lines[i][j] != ' ')){
 						vertice_string+=lines[i][j];
-					}else{
-						vertice_int[pos_vertice] = int.Parse(vertice_string);
-						//Debug.Log(cantF+"-"+pos_vertice+": "+vertice_string);
+					} else{
+						//Debug.Log(cantV+"-"+pos_vertice+": "+vertice_string);
+						vertice_float[pos_vertice] = float.Parse(vertice_string.Split(".")[0]) + float.Parse(vertice_string.Split(".")[1])*Mathf.Pow(10,(-1)*vertice_string.Split(".")[1].Length);
 						pos_vertice++;
 						vertice_string = "";
 					}
 				}
-				vertice_int[pos_vertice] = int.Parse(vertice_string);
-				//Debug.Log(cantF+"-"+pos_vertice+": "+vertice_string);
-				Debug.Log(cantF+": <"+vertice_int[0]+" - "+vertice_int[3]+" - "+vertice_int[6]+">");
+				//Debug.Log(cantV+"-"+pos_vertice+": "+vertice_string+"---");
+				vertice_float[pos_vertice] = float.Parse(vertice_string.Split(".")[0]) + float.Parse(vertice_string.Split(".")[1])*Mathf.Pow(10,(-1)*(vertice_string.Split(".")[1].Length-1));
+				//Debug.Log(cantV+": <"+vertice_float[0]+" - "+vertice_float[1]+" - "+vertice_float[2]+">");
+				lista_vertice.Add(new Vector3(vertice_float[0], vertice_float[1], vertice_float[2]));
+				cantV++;
+			} else if (lines[i].StartsWith("f ")){
+				//es una cara
+				int[] triangulo_int = new int[9];
+				string triangulo_string = " ";
+				int pos_triangulo = 0;
+				for(int j = 2; j< lines[i].Length; j++){
+					if((lines[i][j] != ' ')	 && (lines[i][j] != '/')){
+						triangulo_string+=lines[i][j];
+					}else{
+						triangulo_int[pos_triangulo] = int.Parse(triangulo_string);
+						//Debug.Log(cantF+"-"+pos_triangulo+": "+triangulo_string);
+						pos_triangulo++;
+						triangulo_string = "";
+					}
+				}
+				triangulo_int[pos_triangulo] = int.Parse(triangulo_string);
+				//Debug.Log(cantF+"-"+pos_triangulo+": "+triangulo_string);
+				//Debug.Log(cantF+": <"+triangulo_int[0]+" - "+triangulo_int[3]+" - "+triangulo_int[6]+">");
+				lista_triangulo.Add(new Vector3(triangulo_int[0], triangulo_int[1],  triangulo_int[2]));
 				cantF++;
 			}
 		}
+		Debug.Log ("cantidad de vertices: "+lista_vertice.Count+". cantidad de triangulos: "+lista_triangulo.Count);	
+		vertices = new Vector3[cantV];
+		triangles = new int[3*cantF];
+		//foreach(Vector3 v in lista_vertice){}
+		//foreach(Vector3 v in lista_vertice){}
+			
+		for (int v = 0; v < lista_vertice.Count; v++){
+			vertices[v] = lista_vertice[v];
+		} 
+			
+		for(int f = 0; f < lista_triangulo.Count; f++){
+			int t1 = (int)Mathf.Floor(lista_triangulo[f].x); 
+			int t2 = (int)Mathf.Floor(lista_triangulo[f].y);
+			int t3 = (int)Mathf.Floor(lista_triangulo[f].z);
+			triangles[3*f+0] = t1;
+			triangles[3*f+1] = t2;
+			triangles[3*f+2] = t3;
+			//Debug.Log(f+"--> "+(3*f+0)+": "+t1+", "+(3*f+1)+": "+t2+", "+(3*f+2)+": "+t3);
+		}
 	}
+
+	private void UpdateMesh(){
+        objeto.GetComponent<MeshFilter>().mesh.vertices = vertices;
+        objeto.GetComponent<MeshFilter>().mesh.triangles = triangles;
+    }
+
+    private void CreateMaterial(){
+        Material newMaterial = new Material(Shader.Find("ShaderBasico"));
+        objeto.GetComponent<MeshRenderer>().material = newMaterial;
+    }
 }

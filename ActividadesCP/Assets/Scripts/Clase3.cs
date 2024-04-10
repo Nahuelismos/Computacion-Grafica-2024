a6b9e098323c990734d2cea463b1ef0b97902a7a
Nahuel Chaves <nahuel_chaves_98@hotmail.com>
2024-04-10 10:10:10 -0300

    Clase 3 completa + objectos ingresados

--- ActividadesCP/Assets/Scripts/Clase3.cs
+++ ActividadesCP/Assets/Scripts/Clase3.cs
@@ -6,29 +6,34 @@ using UnityEngine;
 public class Clase3 : MonoBehaviour {
 	
 	[SerializeField] private Vector3[] vertices;
-    [SerializeField] private int[] triangles;
+    [SerializeField] private int[] faces;
 	private Color[] colores;
-    private  GameObject objeto;
-	void Start(){
-		string fileName = "bed1";
+    void Start(){
+		//lista_objetos.Add(createModel("bed1",3,3));
+		//lista_objetos.Add(createModel("bed1"));
+		createModel("Bed1");	
+		createModel("muebles/Bathroom/bath/Bath");
+    }
+	
+	void createModel(string fileName){
+		
 		string path = "Assets/Objects/"+fileName+".obj";
 		StreamReader reader = new StreamReader(path);
 		
 		string fileData = (reader.ReadToEnd());
 		ReadEachLine(fileData);//create model
-		
-		
-		objeto = new GameObject(fileName);
-        objeto.AddComponent<MeshFilter>();
+		GameObject objeto = new GameObject(fileName.Split('/')[fileName.Split('/').Length-1]);
+        //GameObject objeto = new GameObject(fileName);
+		objeto.AddComponent<MeshFilter>();
         objeto.GetComponent<MeshFilter>().mesh = new Mesh();
         objeto.AddComponent<MeshRenderer>();
 		
-		UpdateMesh();
-        CreateMaterial();
+		UpdateMesh(objeto);
+        CreateMaterial(objeto);
 		reader.Close();
 		Debug.Log(fileData);
 		
-    }
+	}
 
     void Update() {
         
@@ -36,14 +41,10 @@ public class Clase3 : MonoBehaviour {
 	
 	void ReadEachLine(string fileData){
 		string[] lines = fileData.Split('\n');
-		int cantV = 0;
-		int cantF = 0;
 		List<Vector3> lista_vertice = new List<Vector3>();
-		List<Vector3> lista_triangulo = new List<Vector3>();
+		List<int> lista_cara = new List<int>();
 		for(int i = 0; i< lines.Length; i++){
-			//Debug.Log(lines[i]);
 			if(lines[i].StartsWith("v ")){
-				//es un vertice_string
 				float[] vertice_float = new float[3];
 				string vertice_string = " ";
 				int pos_vertice = 0;
@@ -51,66 +52,57 @@ public class Clase3 : MonoBehaviour {
 					if((lines[i][j] != ' ')){
 						vertice_string+=lines[i][j];
 					} else{
-						//Debug.Log(cantV+"-"+pos_vertice+": "+vertice_string);
-						vertice_float[pos_vertice] = float.Parse(vertice_string.Split(".")[0]) + float.Parse(vertice_string.Split(".")[1])*Mathf.Pow(10,(-1)*vertice_string.Split(".")[1].Length);
-						pos_vertice++;
+						vertice_float[pos_vertice] = float.Parse(vertice_string.ToString())*Mathf.Pow(10,(-1)*vertice_string.Split(".")[1].Length);
+						//float f1 = float.Parse(vertice_string.Split(".")[0]);
+						//float f2 = float.Parse(vertice_string.Split(".")[1])*Mathf.Pow(10,(-1)*vertice_string.Split(".")[1].Length);
+						//f2+=f1;
+						Debug.Log(lista_vertice.Count+" "+pos_vertice+":"+(float.Parse(vertice_string.ToString())*Mathf.Pow(10,(-1)*vertice_string.Split(".")[1].Length))+".");
+						pos_vertice++; 
 						vertice_string = "";
 					}
 				}
-				//Debug.Log(cantV+"-"+pos_vertice+": "+vertice_string+"---");
-				vertice_float[pos_vertice] = float.Parse(vertice_string.Split(".")[0]) + float.Parse(vertice_string.Split(".")[1])*Mathf.Pow(10,(-1)*(vertice_string.Split(".")[1].Length-1));
-				//Debug.Log(cantV+": <"+vertice_float[0]+" - "+vertice_float[1]+" - "+vertice_float[2]+">");
-				lista_vertice.Add(new Vector3(vertice_float[0], vertice_float[1], vertice_float[2]));
-				cantV++;
+				vertice_float[pos_vertice] = float.Parse(vertice_string.ToString())*Mathf.Pow(10,(-1)*vertice_string.Split(".")[1].Length);
+				Vector3 vertice_nuevo = new Vector3(vertice_float[0], vertice_float[1], vertice_float[2]);
+				lista_vertice.Add(vertice_nuevo);
+				Debug.Log(vertice_nuevo);
 			} else if (lines[i].StartsWith("f ")){
-				//es una cara
-				int[] triangulo_int = new int[9];
-				string triangulo_string = " ";
-				int pos_triangulo = 0;
-				for(int j = 2; j< lines[i].Length; j++){//j=2 si es el cubo, j = 3 si son los normales.
-					if((lines[i][j] != ' ')	 && (lines[i][j] != '/')){
-						triangulo_string+=lines[i][j];
-					}else{
-						triangulo_int[pos_triangulo] = int.Parse(triangulo_string);
-						//Debug.Log(cantF+"-"+pos_triangulo+": "+triangulo_string);
-						pos_triangulo++;
-						triangulo_string = "";
+				//if((lines[i].Split(' ').Length-1) > 3){//resto los extremos
+					lines[i] = lines[i].Replase("f  ","f ");
+					Debug.Log((lines[i].Split(' ').Length-1)+": "+lines[i]);
+					string[] fila_separada = lines[i].Split(' ');
+					Vector3[] valores_separados = new Vector3[fila_separada.Length-1];
+					for(int k = 1; k< fila_separada.Length; k++){
+						valores_separados[k-1] =  new Vector3(int.Parse(fila_separada[k].Split('/')[0])-1,int.Parse(fila_separada[k].Split('/')[1]), int.Parse(fila_separada[k].Split('/')[2]));
 					}
-				}
-				triangulo_int[pos_triangulo] = int.Parse(triangulo_string);
-				//Debug.Log(cantF+"-"+pos_triangulo+": "+triangulo_string);
-				//Debug.Log(cantF+": <"+triangulo_int[0]+" - "+triangulo_int[3]+" - "+triangulo_int[6]+">"); //0,3,6 si es normal, 0,1,2 si es el cubo
-				lista_triangulo.Add(new Vector3(triangulo_int[0]-1, triangulo_int[3]-1,  triangulo_int[6]-1));
-				cantF++;
+					for (int l = 0; l< valores_separados.Length-2; l++){
+						//Debug.Log("v"+0+" v"+(l+1)+" v"+(l+2));
+						lista_cara.Add((int) (valores_separados[0].x));
+						lista_cara.Add((int) (valores_separados[l+1].x));
+						lista_cara.Add((int) (valores_separados[l+2].x));
+						if((lines[i].Split(' ').Length-1) > 3)
+							Debug.Log("se añado a <"+valores_separados[0].x+" - "+valores_separados[l+1].x+" - "+valores_separados[l+2].x+">");
+					}
+				
 			}
 		}
-		Debug.Log ("cantidad de vertices: "+lista_vertice.Count+". cantidad de triangulos: "+lista_triangulo.Count);	
-		vertices = new Vector3[cantV];
-		triangles = new int[3*cantF];
-		//foreach(Vector3 v in lista_vertice){}
-		//foreach(Vector3 v in lista_vertice){}
+		Debug.Log ("cantidad de vertices: "+lista_vertice.Count+". cantidad de caras: "+(lista_cara.Count/3));	
+		vertices = new Vector3[lista_vertice.Count];
+		faces = new int[lista_cara.Count];
 			
 		for (int v = 0; v < lista_vertice.Count; v++){
 			vertices[v] = lista_vertice[v];
 		} 
-			
-		for(int f = 0; f < lista_triangulo.Count; f++){
-			int t1 = (int)Mathf.Floor(lista_triangulo[f].x); 
-			int t2 = (int)Mathf.Floor(lista_triangulo[f].y);
-			int t3 = (int)Mathf.Floor(lista_triangulo[f].z);
-			triangles[3*f+0] = t1;
-			triangles[3*f+1] = t2;
-			triangles[3*f+2] = t3;
-			//Debug.Log(f+"--> "+(3*f+0)+": "+t1+", "+(3*f+1)+": "+t2+", "+(3*f+2)+": "+t3);
+		for (int f = 0; f < lista_cara.Count; f++){
+			faces[f] = lista_cara[f];
 		}
 	}
 
-	private void UpdateMesh(){
+	private void UpdateMesh(GameObject objeto){
         objeto.GetComponent<MeshFilter>().mesh.vertices = vertices;
-        objeto.GetComponent<MeshFilter>().mesh.triangles = triangles;
+        objeto.GetComponent<MeshFilter>().mesh.triangles = faces;
     }
 
-    private void CreateMaterial(){
+    private void CreateMaterial(GameObject objeto){
         Material newMaterial = new Material(Shader.Find("ShaderBasico"));
         objeto.GetComponent<MeshRenderer>().material = newMaterial;
     }
